f0c9310e70366b91aab5c1da37b8a1216d6cdbc3
Luiza F Neves <luizamariana888@gmail.com>
2023-02-26 02:55:18 -0300

    concertando menu hamburguer

--- Site_Pocketim/js/scripts.js
+++ Site_Pocketim/js/scripts.js
@@ -4,23 +4,38 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("scrolling-active", windowPosition);
 });
 
+const menu = document.getElementById('menu')
+
+$(document).ready(function(){
+    console.log(menu.classList)
+    $("#barras").click(function(){
+       if ($("#menu").hasClass("menu-ativo")){
+            $("#menu").removeClass("menu-ativo")
+       }else{
+        $("#menu").addClass("menu-ativo")
+       }
+        
+        
+    })
+})
+
 function cadastrarNewsletter(){
     let email = document.getElementById("campo-email").value
     alert(email)
 
 }
 
-function mostrarMenu($event){
+//function mostrarMenu($event){
 
-    let menu = document.getElementById('menu')
+   // let menu = document.getElementById('menu')
 
-    if (getComputedStyle(menu).display =="none"){
-        menu.style.display = "flex"
-    }else{
-        if (window.screen.width > 1000) {
-            menu.style.display = "none"
-        }
-    }
+   // if (getComputedStyle(menu).display =="none"){
+  //      menu.style.display = "flex"
+  //  }else{
+  //      if (window.screen.width > 1000) {
+  //          menu.style.display = "none"
+  //      }
+  //  }
 
-    event.preventDefault()
-}
+  //  event.preventDefault()
+//}
